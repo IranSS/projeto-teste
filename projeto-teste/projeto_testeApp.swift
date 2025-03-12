6cbbcf62eb20c9c4225e1ac551ad810c3662744c
Iranildo Sousa <iranildosousa1234@gmail.com>
2025-03-12 16:51:57 -0300

    Initial Commit

---
+++ projeto-teste/projeto_testeApp.swift
@@ -0,0 +1,17 @@
+//
+//  projeto_testeApp.swift
+//  projeto-teste
+//
+//  Created by user on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct projeto_testeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
